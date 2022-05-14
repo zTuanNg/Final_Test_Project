d3af7b94ac445ef3ef0dfe2db9d424c910b179fa
zTuanNg <tuann.nm241@gmail.com>
2022-05-15 02:23:23 +0700

    update

--- Banking/src/main/java/AccountController.java
+++ Banking/src/main/java/AccountController.java
@@ -8,26 +8,27 @@ public class AccountController {
     static String password;
     static Account currentAccount;
 
-    public void run(){
+    public static void run() {
+        System.out.println("Đọc readme để lấy thông tin account");
         login();
-        currentAccount = service.getAccountByUsernameAndPassword(username,password);
+        currentAccount = service.getAccountByUsernameAndPassword(username, password);
 
-        while(true){
+        while (true) {
             menu();
             System.out.println("Enter your choice: ");
             int choice;
-            try{
+            try {
                 choice = Integer.parseInt(sc.nextLine());
-            }catch (Exception e){
+            } catch (Exception e) {
                 System.out.println("Please enter integer number");
                 continue;
             }
 
-            switch(choice){
+            switch (choice) {
 
                 // case1 : Truy vấn số dư tài khoản
                 case 1:
-                    System.out.println("Balance: "+service.getBalance(currentAccount));
+                    System.out.println("=>Balance: " + service.getBalance(currentAccount));
                     break;
 
                 // Chuyển tiền
@@ -50,13 +51,13 @@ public class AccountController {
     }
 
 
-    public static void login(){
-        while (true){
+    public static void login() {
+        while (true) {
             System.out.println("Enter Username: ");
             String name = sc.nextLine();
             System.out.println("Enter password: ");
             String pass = sc.nextLine();
-            if(service.checkValid(name,pass)){
+            if (service.checkValid(name, pass)) {
                 username = name;
                 password = pass;
                 System.out.println("Login successful !!!");
@@ -66,7 +67,8 @@ public class AccountController {
         }
     }
 
-    public static void menu(){
+    public static void menu() {
+
         System.out.println("Welcome, Please choice this option");
         System.out.println("1 - Show balance");
         System.out.println("2 - Withdraw");
@@ -74,74 +76,73 @@ public class AccountController {
         System.out.println("0 - Exit");
     }
 
-    public static boolean checkValidSTK(String stk){
-        if(stk.length()==8 || stk.length()==15){
-            try{
-                int i =0;
-                while(i < stk.length()){
-                    int num = Integer.parseInt(stk.substring(i,i+1));
+    public static boolean checkValidSTK(String stk) {
+        if (stk.length() == 8 || stk.length() == 15) {
+            try {
+                int i = 0;
+                while (i < stk.length()) {
+                    int num = Integer.parseInt(stk.substring(i, i + 1));
                     i++;
                 }
                 return true;
-            }catch(Exception e){
+            } catch (Exception e) {
                 return false;
             }
-        }else {
+        } else {
             return false;
         }
     }
 
     // Input STK
-    public static Account inputSTK(Account account){
-        while(true){
+    public static Account inputSTK(Account account) {
+        while (true) {
             System.out.println("Enter account number: ");
             String stk = sc.nextLine();
 
-            if(account.getAccountNumber().equals(stk)){
+            if (account.getAccountNumber().equals(stk)) {
                 System.out.println("This account number is yours, Please enter different account number");
                 continue;
             }
-            if(!checkValidSTK(stk)){
+            if (!checkValidSTK(stk)) {
                 System.out.println("Account number must be 8 or 15 characters!!!");
                 System.out.println("Please try again!!!");
                 continue;
             }
 
             Account acc = service.getAccountBySTK(stk);
-            if(acc != null){
+            if (acc != null) {
                 return acc;
-            }else{
+            } else {
                 System.out.println("This account number is not exist, Please try again!!!");
             }
         }
     }
 
     // transaction
-    public static void transaction( Account currentAccount){
+    public static void transaction(Account currentAccount) {
         Account acc = inputSTK(currentAccount);
 
-        while(true){
+        while (true) {
             double amount;
             System.out.println("Enter amount: ");
-            try{
+            try {
                 amount = Double.parseDouble(sc.nextLine());
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 System.out.println("Amount is not valid, try again!!!");
                 continue;
             }
 
-            if(currentAccount.getBalance() < 100000){
+            if (currentAccount.getBalance() < 100000) {
                 System.out.println("Balance is not enough to do transaction !!!");
                 break;
             }
 
-            if(amount<50000){
+            if (amount < 50000) {
                 System.out.println("Amount must be > 50000, Try agian!!!");
                 continue;
             }
-            if(currentAccount.getBalance()-amount < 50000){
-                System.out.printf("Your balance is: %s, amount too much , Please try agian!!!\n",service.getBalance(currentAccount));
+            if (currentAccount.getBalance() - amount < 50000) {
+                System.out.printf("Your balance is: %s, amount too much , Please try agian!!!\n", service.getBalance(currentAccount));
                 continue;
             }
 
@@ -149,7 +150,7 @@ public class AccountController {
             String message = sc.nextLine();
             currentAccount.withdraw(amount);
             System.out.println("Transaction successful");
-            Transaction trans = new Transaction(message,acc.getAccountNumber(),amount);
+            Transaction trans = new Transaction(message, acc.getAccountNumber(), amount);
             List<Transaction> t = currentAccount.getTransaction();
             t.add(trans);
             break;
@@ -157,11 +158,12 @@ public class AccountController {
         }
     }
 
-    public void showHistoryTransition(Account acc){
+    public static void showHistoryTransition(Account acc) {
         System.out.println("===History transaction===");
-        acc.getTransaction().forEach(t-> System.out.println(t));
+        if (acc.getTransaction().size() != 0)
+            acc.getTransaction().forEach(t -> System.out.println(t));
+        else
+            System.out.println("=>History transaction is empty!");
 
     }
-
-
 }
--- Banking/src/main/java/App.java
+++ Banking/src/main/java/App.java
@@ -6,8 +6,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
 
-        AccountController controller = new AccountController();
-        controller.run();
+        AccountController.run();
 
     }
 }
